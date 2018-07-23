5e76470c84d806340bcfa601db081dd6c152683c
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2018-07-23 08:38:28 +0200

    Update pat_speeder.php
    Check if zlib compression exists into the server config and avoids document formatting errors.

--- pat_speeder.php
+++ pat_speeder.php
@@ -78,7 +78,7 @@ function _pat_speeder_go($buffer, $gzip, $code, $compact)
 	$buffer = preg_replace('/<!--([^<|\[|>|go{2}gleo]).*?-->/s', '', $buffer);
 
 	// Server side compression if available
-	if (get_pref('pat_speeder_gzip') and $gzip && isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+	if (get_pref('pat_speeder_gzip') and $gzip && (isset($_SERVER['HTTP_ACCEPT_ENCODING']) and ini_get('zlib.output_compression'))) {
 		$encoding = $_SERVER['HTTP_ACCEPT_ENCODING'];
 		if (function_exists('gzencode') && preg_match('/gzip/i', $encoding)) {
 			header('Content-Encoding: gzip');
